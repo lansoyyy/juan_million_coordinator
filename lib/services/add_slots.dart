03da2c734854918b9b678432c72bc4bb47c8f0a9
Lans12345 <olanalans12345@gmail.com>
2026-03-04 09:15:04 +0800

    update

--- lib/services/add_slots.dart
+++ lib/services/add_slots.dart
@@ -1,14 +1,14 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
-Future<String> addSlots() async {
+Future<String> addSlots({DateTime? dateTime}) async {
   final docUser = FirebaseFirestore.instance
       .collection('Slots')
       .doc(); // auto-generated ID ensures uniqueness
 
   final json = {
     'uid': FirebaseAuth.instance.currentUser!.uid,
-    'dateTime': DateTime.now(),
+    'dateTime': dateTime ?? DateTime.now(),
   };
 
   await docUser.set(json);
